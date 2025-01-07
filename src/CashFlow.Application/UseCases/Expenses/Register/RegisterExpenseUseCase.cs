07e49cc96d8bd978bac0acbcc8ab58d1e2b7902f
Lucas Silva <lucasstrlisilva@outlook.com>
2025-01-07 20:12:51 -0300

    Add project files.

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredExpense Execute(RequestRegisterExpense request)
+        {
+            return new ResponseRegisteredExpense();
+        }
+    }
+}
