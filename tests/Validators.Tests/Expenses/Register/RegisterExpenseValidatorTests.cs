3b9613c16fd4503977fdba7b9296a9ff9ad765f1
Lucas Silva <lucasstrlisilva@outlook.com>
2025-01-23 16:48:54 -0300

    criado projetos de testes

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+
+        public void Success()
+        {
+            //Arrange configuracao, instanciacao das propriedades para executar o teste
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build(); //usando bpgus para mockar as requisicoes
+
+            //Act executa a chamada ao metodo que vai ser testado
+            var result = validator.Validate(request);
+
+            //Assert compara o resultado para gerar o retorno final, se o teste passou ou falhou
+            result.IsValid.Should().BeTrue(); //usando fluentassertions para fazer as comparacoes
+        }
+    }
+}
