b66f5a1fc8c3dfdd6beaf4084ba92f94d48055fd
Lucas Silva <lucasstrlisilva@outlook.com>
2025-01-24 13:59:46 -0300

    adiconado novos testes para validators.

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
-using CashFlow.Communication.Requests;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -20,6 +21,77 @@ namespace Validators.Tests.Expenses.Register
 
             //Assert compara o resultado para gerar o retorno final, se o teste passou ou falhou
             result.IsValid.Should().BeTrue(); //usando fluentassertions para fazer as comparacoes
+        }
+
+        [Fact]
+
+        public void ErrorTitleEmpty ()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Title = string.Empty;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                  .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_IS_REQUIRED));
+        }
+
+        [Fact]
+        public void ErrorDateFuture()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Date = DateTime.UtcNow.AddDays(1);
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                  .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_BE_FOR_THE_FUTURE));
+        }
+
+        [Fact]
+        public void ErrorPaymentTypeInvalid()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.PaymentType = (PaymentType)700;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                  .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_IS_NOT_VALID));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void ErrorAmountInvalid(decimal amount)
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Amount = amount;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                  .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
         }
     }
 }
