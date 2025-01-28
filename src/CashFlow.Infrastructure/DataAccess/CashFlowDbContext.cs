ef47d5108609f07675daea01064f664693d6e2c3
Lucas Silva <lucasstrlisilva@outlook.com>
2025-01-28 17:56:10 -0300

    add db context

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=L190u3c20s%";
+            var serverVersion = new MySqlServerVersion(new Version());
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
