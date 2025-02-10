af6178170cba4a256f9b8b433d5c84f322ed1461
Lucas Silva <lucasstrlisilva@outlook.com>
2025-02-10 10:55:54 -0300

    configurado e implementado o auto mapper

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers
@@ -9,6 +10,8 @@ namespace CashFlow.Api.Controllers
     public class ExpensesController : ControllerBase
     {
         [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredExpense), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseError), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Register([FromBody] RequestRegisterExpense request, [FromServices] IRegisterExpenseUseCase useCase)
         {
             var response =  await useCase.Execute(request);
--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
@@ -11,29 +12,25 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         private readonly IExepensesRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
-        public RegisterExpenseUseCase(IExepensesRepository repository, IUnitOfWork unitOfWork)
+        private readonly IMapper _mapper;
+
+        public RegisterExpenseUseCase(IExepensesRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
         {
             _repository = repository;
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
         public async Task<ResponseRegisteredExpense> Execute(RequestRegisterExpense request)
         {
             Validate(request);
 
-            var entity = new Expense
-            {
-                Amount = request.Amount,
-                Date = request.Date,
-                Description = request.Description,
-                Title = request.Title,
-                PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
-            };
+            var entity = _mapper.Map<Expense>(request);
 
             await _repository.Add(entity);
 
             await _unitOfWork.Commit();
 
-            return new ResponseRegisteredExpense();
+            return _mapper.Map<ResponseRegisteredExpense>(entity);
         }
 
         private void Validate(RequestRegisterExpense request)
