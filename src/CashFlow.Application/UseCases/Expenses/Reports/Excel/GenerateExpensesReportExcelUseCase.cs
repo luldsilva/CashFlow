c7da20cfdbbb20be9a29860df6b9e29096aaa038
Lucas Silva <lucasstrlisilva@outlook.com>
2025-02-18 15:36:16 -0300

    criado modelo de excel, criados arquivos de resource para o report, criado filtro de despesa por data no repositorio de despesas

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,11 +1,22 @@
 ﻿using ClosedXML.Excel;
 
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
+
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
 {
     public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
     {
+        private readonly IExpensesReadOnlyRepository _repository;
+
+        public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
+        {
+            _repository = repository;
+        }
         public async Task<byte[]> Execute(DateOnly month)
         {
+            var expenses = await _repository.FilterByMonth(month);
+
             var workbook = new XLWorkbook();
 
             workbook.Author = "Lucas Silva";
@@ -13,6 +24,28 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
             workbook.Style.Font.FontName = "Times New Roman";
 
             var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+
+            InsertHeader(worksheet);
+
+            var file = new MemoryStream();
+            workbook.SaveAs(file);
+            
+            return file.ToArray();
+        }
+
+        private void InsertHeader(IXLWorksheet worksheet)
+        {
+            worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
+            worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
+            worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+            worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
+            worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION; 
+
+            worksheet.Cells("A1:E1").Style.Font.Bold = true;
+
+            worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
+
+            worksheet.Cells("A1:E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
         }
     }
 }
--- src/CashFlow.Infrastructure/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/Repositories/ExpensesRepository.cs
@@ -13,7 +13,7 @@ namespace CashFlow.Infrastructure.Repositories
             _dbContext = dbContext;
         }
         public async Task Add(Expense expense)
-        {           
+        {
             await _dbContext.Expenses.AddAsync(expense);
         }
 
@@ -21,7 +21,7 @@ namespace CashFlow.Infrastructure.Repositories
         {
             var result = await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
 
-            if(result is null)
+            if (result is null)
             {
                 return false;
             }
@@ -52,6 +52,22 @@ namespace CashFlow.Infrastructure.Repositories
         public void Update(Expense expense)
         {
             _dbContext.Expenses.Update(expense);
+        }
+
+        public async Task<List<Expense>> FilterByMonth(DateOnly date)
+        {
+            var startDate = new DateTime(year: date.Year, month: date.Month, day: 1).Date;
+
+            var daysInMonth = DateTime.DaysInMonth(year: date.Year, month: date.Month);
+
+            var endDate = new DateTime(year: date.Year, month: date.Month, day: daysInMonth, hour: 23, minute: 59, second: 59);
+
+            return await _dbContext.Expenses
+                .AsNoTracking()
+                .Where(e => e.Date > startDate && e.Date <= endDate)
+                .OrderBy(e => e.Date)
+                .ThenBy(e => e.Title)
+                .ToListAsync();
         }
     }
 }
