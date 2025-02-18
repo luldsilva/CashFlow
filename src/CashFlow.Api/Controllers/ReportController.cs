701c838057cc72fbd36f3fcfb0fc9a3e04ed4b31
Lucas Silva <lucasstrlisilva@outlook.com>
2025-02-18 08:56:02 -0300

    criado endpoint e request para devolucao de relatorio em excel

---
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : Controller
+    {
+        [HttpGet("excel")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetExcel([FromHeader] DateOnly month)
+        {
+            //usar o from header e dateOnly em um metodo get, faz sentido se poucos filtros
+            //se forem muitos filtros faz sentido passar no body e mudar o metodo para post
+            byte[] file = new byte[1];
+
+            if(file.Length > 0)
+                return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+            
+            return NoContent();
+        }
+    }
+}
