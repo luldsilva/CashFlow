81028d99d9f4dc59110b28fb2b57f9c1f67ccc7e
Lucas Silva <lucasstrlisilva@outlook.com>
2025-02-24 11:40:15 -0300

    criadas funcoes para renderizacao do pdf e arquivos de resource

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,5 +1,8 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
@@ -24,7 +27,81 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
                 return [];
             }
 
-            return [];
+            var document = CreateDocument(month);
+            var page = CreatePage(document);
+
+
+            var table = page.AddTable();
+            table.AddColumn();
+            table.AddColumn("300");
+
+            var row = table.AddRow();
+
+            row.Cells[0].AddImage("");
+
+            row.Cells[1].AddParagraph("Hey, Lucas Silva");
+            row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+            row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
+
+
+            var paragraph = page.AddParagraph();
+            paragraph.Format.SpaceBefore = "40";
+            paragraph.Format.SpaceAfter = "40";
+
+            var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+
+            paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALEWAY_REGULAR, Size = 15});
+
+            paragraph.AddLineBreak();
+
+            var totalValueExpenses = expenses.Sum(e => e.Amount);
+            paragraph.AddFormattedText($"{totalValueExpenses} {CURRENCY_SYMBOL}", 
+                new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50});
+
+
+            return RenderDocument(document);
+        }
+
+        private Document CreateDocument(DateOnly month)
+        {
+            var document = new Document();
+
+            document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSE_FOR} {month:Y}";
+            document.Info.Author = "Lucas Silva";
+
+            var style = document.Styles["Normal"];
+            style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+            return document;
+        }
+        private Section CreatePage(Document document)
+        {
+            var section = document.AddSection();
+            section.PageSetup = document.DefaultPageSetup.Clone();
+
+            section.PageSetup.PageFormat = PageFormat.A4;
+
+            section.PageSetup.LeftMargin = 40;
+            section.PageSetup.RightMargin = 40;
+            section.PageSetup.TopMargin = 80;
+            section.PageSetup.BottomMargin = 80;
+
+            return section;
+        }
+
+        private byte[] RenderDocument(Document document)
+        {
+            var renderer = new PdfDocumentRenderer
+            {
+                Document = document,
+            };
+
+            renderer.RenderDocument();
+
+            using var file = new MemoryStream();
+            renderer.PdfDocument.Save(file);
+
+            return file.ToArray();
         }
     }
 }
