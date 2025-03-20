2f0d4f85e20aa7552a281ec630056142965080a5
Lucas Silva <lucasstrlisilva@outlook.com>
2025-03-20 19:09:44 -0300

    criado teste unitario para registro de usuario

---
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,54 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Users.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void Error_Name_Empty(string name)
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserBuilder.Build();
+            request.Name = name;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.NAME_EMPTY));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void Error_Email_Empty(string email)
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserBuilder.Build();
+            request.Email = email;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_EMPTY));
+        }
+
+    }
+}
